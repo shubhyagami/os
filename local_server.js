d89150e7b4b2957a2cfe94995d8ffb1e4eb4a271
shubhyagami <shubhyagami@users.noreply.github.com>
2026-07-19 18:38:12 +0530

    Use PORT env var for Render compatibility

--- local_server.js
+++ local_server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const MIME_TYPES = {
 ".html": "text/html; charset=utf-8",
